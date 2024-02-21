8af67c7d90479092770ee2625b1f00d083a19dfc
rachityadav730 <rachit.r.yadav@gmail.com>
2024-02-21 21:35:17 +0530

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { StrictMode } from 'react';
+import PropType from 'prop-types';
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+// import { PersistGate } from 'redux-persist/integration/react';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>
-      Ecommerce site
-     </h1>
-    </div>
-  );
-}
+
+const App = ({ store, persistor }) => (
+  <StrictMode>
+    <Provider store={store}>
+      {/* <PersistGate loading={<Preloader />} persistor={persistor}> */}
+        <AppRouter />
+      {/* </PersistGate> */}
+    </Provider>
+  </StrictMode>
+);
+
+App.propTypes = {
+  store: PropType.any.isRequired,
+  persistor: PropType.any.isRequired
+};
 
 export default App;
